d30d92025fa5e17d7f891aff8e8a41ecf0154e71
Plenitude-ai <axeldurandfontanel@hotmail.fr>
2025-08-05 13:51:46 +0200

    feat: jar filesystem view fix

--- src/main/js/components/file-explorer/file-explorer.js
+++ src/main/js/components/file-explorer/file-explorer.js
@@ -1,26 +1,31 @@
 import React, {useState} from 'react';
 
 function FileExplorer({node}) {
-    const [expanded, setExpanded] = useState(false);
+    const [dirExpanded, setDirExpanded] = useState(true);
+    const [fileExpanded, setFileExpanded] = useState(false);
 
-    if (node.type === 'file') {
+    if (node.file) {
         return (
-            <div className="pl-4 text-white">
-                📄 {node.name}
-                <pre className="bg-gray-900 p-2 mt-1 text-sm">{node.content}</pre>
+            <div className="pl-4 text-white" >
+                <div className="cursor-pointer" onClick={() => setFileExpanded(!fileExpanded)}>
+                    📄 {node.name}
+                </div>
+                {fileExpanded && (
+                    <pre className="bg-gray-900 p-2 mt-1 text-sm">{node.content}</pre>
+                )}
             </div>
         );
     }
 
     return (
         <div className="pl-4 text-white">
-            <div className="cursor-pointer" onClick={() => setExpanded(!expanded)}>
+            <div className="cursor-pointer" onClick={() => setDirExpanded(!dirExpanded)}> 
                 📁 {node.name}
             </div>
-            {expanded && node.children && (
+            {dirExpanded && node.children && (
                 <div className="ml-4 border-l border-gray-600 pl-2">
-                    {node.children.map((child, index) => (
-                        <FileExplorer key={index} node={child}/>
+                    {Object.entries(node.children).map(([key, childNode]) => (
+                        <FileExplorer key={key} node={childNode} />
                     ))}
                 </div>
             )}
--- src/main/js/routes/apppackage/app-package.js
+++ src/main/js/routes/apppackage/app-package.js
@@ -99,7 +99,7 @@ function AppPackage() {
     if (javaFs) {
         tabsContent.push({
             "tabName": javaFs.componentsJarName,
-            "payload": javaFs,
+            "payload": javaFs.root,
             "contentType": "filesystem"
         });
     }
